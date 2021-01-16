18cba043d135088a532697f5a595d75771cc9e59
Becky Schmitthenner <becky.schmitthenner@gmail.com>
2021-01-16 15:41:46 -0500

    first routes & then added router

---
+++ fakeDatabase.js
@@ -0,0 +1,3 @@
+global.ITEMS = [];
+
+module.exports = ITEMS;
--- itemRoutes.js
+++ itemRoutes.js
@@ -1,47 +1,33 @@
 const express = require("express");
-const ITEMS = require("./fakeDatabase");
-const itemRoutes = require("./itemRoutes");
+const router = new express.Router();
 
-const app = express()
-
-app.use(express.json())
-app.use("/items", itemRoutes)
-
-app.get('/', (req, res, next) => {
+router.get('/', (req, res, next) => {
     return res.json(ITEMS);
 })
 
-app.get('/:name', (req, res, next) => {
+router.get('/:name', (req, res, next) => {
     const item = ITEMS.find(i => i.name === req.params.name);
     return res.json(item);
 })
 
-app.post('/', (req, res, next) => {
+router.post('/', (req, res, next) => {
     const newItem = req.body;
     items.push(newItem);
     return res.json({added: newItem});
 })
 
-app.patch('/:name', (req, res, next) => {
+router.patch('/:name', (req, res, next) => {
     const item = ITEMS.find(i => i.name === req.params.name);
     item.name = req.body.name;
     item.price = req.body.price;
     return res.json({updated: item})
 })
 
-app.delete('/:name', (req, res, next) => {
+router.delete('/:name', (req, res, next) => {
     const item = ITEMS.find(i => i.name === req.params.name);
     ITEMS.splice(item, 1)
     return res.json({message: "deleted"})
 })
 
 
-app.listen(3000, () => {
-    console.log("starting server on port 3000")
-})
-
-let milk = {
-    name: "milk",
-    price: "3.45"
-}
-ITEMS.push(milk)
+module.exports = router;
